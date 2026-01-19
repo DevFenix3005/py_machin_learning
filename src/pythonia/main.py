47c7c755f717fa8d172b0e95687ae815a2ed9b4b
Roberto D. Cazarin <robertodmngzczrn@gmail.com>
2026-01-19 14:40:39 -0600

    Add joblib and scikit-learn dependencies; implement KNN classifier in main.py

--- src/pythonia/main.py
+++ src/pythonia/main.py
@@ -1,4 +1,8 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import confusion_matrix, accuracy_score
 
 
 def fill_age(row):
@@ -24,12 +28,33 @@ if __name__ == "__main__":
     age_3 = df[df["Pclass"] == 3]["Age"].median()
 
     df.drop(["PassengerId", "Name", "Ticket", "Cabin"], axis=1, inplace=True)
-    df["Embarked"].fillna("S", inplace=True)
+    df.fillna({"Embarked": "S"}, inplace=True)
     df["Age"] = df.apply(fill_age, axis=1)
     df["Sex"] = df["Sex"].apply(fill_sex)
 
     df[list(pd.get_dummies(df["Embarked"]).columns)] = pd.get_dummies(df["Embarked"])
     df.drop("Embarked", axis=1, inplace=True)
 
-    df.info()
-    print(df.head())
+    x = df.drop("Survived", axis=1)  # Datos del pasajero
+    y = df["Survived"]  # Variable objetivo
+
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.10)
+
+    sc = StandardScaler()
+    x_train = sc.fit_transform(x_train)
+    x_test = sc.transform(x_test)
+
+    classifier = KNeighborsClassifier(n_neighbors=3)
+    classifier.fit(x_train, y_train)
+
+    y_pred = classifier.predict(x_test)
+    percent = accuracy_score(y_test, y_pred) * 100
+    matrix = confusion_matrix(y_test, y_pred)
+
+    print(percent)
+    print(matrix)
+
+    for caracteristica in x_test:
+        for valor in x_test[caracteristica]:
+            print(valor)
+
